3de25bf5bcd4411ef1583d7c2ae9fbfa49cdada7
nj7wrr4fz5-cloud <nj7wrr4fz5@privaterelay.appleid.com>
2026-04-01 23:49:59 +0300

    Обновляю импорт Routes -> AppRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { BrowserRouter } from 'react-router-dom'
-import Routes from './routes'
+import AppRoutes from './routes'
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes />
+      <AppRoutes />
     </BrowserRouter>
   )
 }
