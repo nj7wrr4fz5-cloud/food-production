c4fe052bf0a158e701dfa4553397d52b879931c4
nj7wrr4fz5-cloud <nj7wrr4fz5@privaterelay.appleid.com>
2026-04-01 23:49:47 +0300

    Исправляю конфликт имен - переименовываю Routes в AppRoutes

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 
-export default function Routes() {
+export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
